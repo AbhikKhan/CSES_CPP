fa4859505a3e1eaf5e92f02c8a2e651d585be01a
Abhik Khan <abhik.khan.1997@gmail.com>
2023-09-29 12:31:51 +0530

    Addede grid path

--- Introductory Problems/1625gridPath.cpp
+++ Introductory Problems/1625gridPath.cpp
@@ -1,94 +1,70 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define isValid(x, a, b) (x>=a && x< b? 1: 0) 
 
-#define speed_ ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
 
-#define ll long long
-#define ld long double
+int vis[7][7];
 
-#define vt vector
-#define pb push_back
+#define RIGHT 0
+#define LEFT 1
+#define DOWN 2
+#define UP 3
 
-#define all(x) (x).begin(), (x).end()
-#define fact(n) tgamma(n + 1)
+string grid;
 
-
-/* Trie */
-struct Trie{
-    bool isPresent;
-    Trie *next[26];
-
-    Trie(){
-        memset(next, 0, sizeof(next));
-        isPresent = false;
+int findPath(int x, int y, int pos){
+    if(pos == 48){
+        return x == 6 && y == 0;
     }
-};
-int res = 0;
-vector<int> dir = {-1, 0, 1, 0, -1};
-void findPath(int x, int y, string& path, string& grid, vector<bool>& vis, int ind = 0){
-    if(path.length()> 48)return;
-    if(x == 6 && y == 0){
-        if(path.length() == 48){
-            cout<<path<<endl;
-            res++;
-        }
-        return;
+    if(x == 6 && y == 0)return 0;
+    if(vis[x][y])return 0;
+
+    vector<bool> visited(4, -1); // to keep track of neighbouring vis/unvisited cells
+    for(int i = 0; i< 4; ++i){
+        if(isValid(x+dx[i],0,7) && isValid(y+dy[i],0,7))
+            visited[i] = vis[x+dx[i]][y+dy[i]]; // U R D L
     }
-    if(x == 0 && y == 0){
-        vis[0] = true;
-        path+="D";
-        findPath(x+1, y, path, grid, vis, ind+1);
-    }
-    else{
-        // collision with wall or we can say the current position divides the grid
-        // to two parts having not visited cells
-        
-        vector<char> move = {'U', 'R', 'D', 'L'};
+    if(visited[UP] && visited[DOWN] && !visited[LEFT] && !visited[RIGHT])return 0; // collision to wall
+    if(!visited[UP] && !visited[DOWN] && visited[LEFT] && visited[RIGHT])return 0; // or collision with other visited cell
+
+    // corner cases
+    /*
+        [V][N]  [c][N]  [N][V]  [N][c]  N: not visited, V: visited, c: current Pos
+        [N][c]  [N][V]  [c][N]  [V][N]
+    */
+    if(isValid(x-1,0,7) && isValid(y+1,0,7) && vis[x-1][y+1])
+        if(!visited[RIGHT] && !visited[UP])return 0;
+    if(isValid(x+1,0,7) && isValid(y+1,0,7) && vis[x+1][y+1])
+        if(!visited[RIGHT] && !visited[DOWN])return 0;
+    if(isValid(x-1,0,7) && isValid(y-1,0,7) && vis[x-1][y-1])
+        if(!visited[LEFT] && !visited[UP])return 0;
+    if(isValid(x+1,0,7) && isValid(y-1,0,7) && vis[x+1][y-1])
+        if(!visited[LEFT] && !visited[DOWN])return 0;
+    
+    vis[x][y] = 1;
+    int res = 0;
+    if(grid[pos] == '?'){
         for(int i = 0; i< 4; ++i){
-            int X = x+dir[i], Y = y+dir[i+1];
-            if(min(X, Y)< 0 || max(X, Y)>= 7 || vis[X*7+Y] || (grid[ind] != '?' && move[i] != grid[ind]))continue;
-
-            int xx = X, yy = Y; // one step ahead
-            if(move[i] == 'L')yy--;
-            else if(move[i] == 'R')yy++;
-            else if(move[i] == 'U')xx--;
-            else xx++;
-
-            if(min(xx, yy)< 0 || max(xx, yy)>= 7 || vis[7*xx+yy]){
-                if(path.back() == 'R' || path.back() == 'L'){
-                    if(X+1< 7 && X-1>= 0 && !vis[(7*(X+1)+Y)] && !vis[7*(X-1)+Y])continue;
-                }
-                else{
-                    if(Y+1< 7 && Y-1>= 0 && !vis[(7*X+Y+1)] && !vis[7*X+Y-1])continue;
-                }
-            }
-
-            path += move[i];
-            vis[X*7+Y] = true;
-            findPath(X, Y, path, grid, vis, ind+1);
-            vis[X*7+Y] = false;
-            path.pop_back();
+            if(isValid(x+dx[i], 0, 7) && isValid(y+dy[i], 0, 7))
+                res += findPath(x+dx[i], y+dy[i], pos+1);
         }
     }
-}
-
-void solve(){
-    string grid = "";
-    cin>>grid;
-    vector<bool> vis(49);
-    string path = "";
-    findPath(0, 0, path, grid, vis);
-    cout<<res*2<<endl;
+    else if(grid[pos] == 'U' && x-1 >= 0)
+            res += findPath(x-1, y, pos+1);
+    else if(grid[pos] == 'R' && y+1 < 7)
+            res += findPath(x, y+1, pos+1);
+    else if(grid[pos] == 'D' && x+1 < 7)
+            res += findPath(x+1, y, pos+1);
+    else if(grid[pos] == 'L' && y-1 >= 0)
+            res += findPath(x, y-1, pos+1);
+    vis[x][y] = 0;
+    return res;
 }
 
 int main(){
-    speed_;
-    ll t = 1;
-    // cin>>t;
-
-    while (t--){
-        solve();
-    }
-    
+    cin>>grid;
+    cout<<findPath(0,0,0)<<endl;
     return 0;
 }
